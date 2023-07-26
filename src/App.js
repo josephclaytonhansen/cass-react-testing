a92427fd9e104174adbef489f8405112c84b181d
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-07-25 21:21:32 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch, withRouter  } from 'react-router-dom'
+import Home from './screens/HomeScreen'
+
+function App() {
+  
+  
+  return (
+    <>
+
+
+    <Router>
+        <Switch>
+
+          <Route path="/" component={withRouter(Home)} exact index={true} />
+          
+        </Switch>
+        
+    </Router>
+
+    
+    </>
+  )
+}
+
+export default App
